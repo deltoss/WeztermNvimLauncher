298d6458cdc297ea9a1308e2f5af9dae564ea046
Michael Tran <mtran0011@outlook.com>
2025-07-21 11:25:39 +1000

    Fix

--- Install-App.ps1
+++ Install-App.ps1
@@ -1,3 +1,5 @@
+$scriptDir = $PSScriptRoot
+
 # Check if ps2exe module is installed
 if (Get-Module -ListAvailable -Name ps2exe) {
   Write-Host "ps2exe module is already installed." -ForegroundColor Green
@@ -11,12 +13,12 @@ if (Get-Module -ListAvailable -Name ps2exe) {
   }
 }
 
-ps2exe "$PSScriptRoot/WeztermNeovimLauncher.ps1" "$PSScriptRoot/output/WeztermNeovimLauncher.exe" -noConsole -iconFile "$PSScriptRoot/assets/Icon.ico" -longPaths `
+ps2exe "$scriptDir/WeztermNeovimLauncher.ps1" "$scriptDir/output/WeztermNeovimLauncher.exe" -noConsole -iconFile "$scriptDir/assets/Icon.ico" -longPaths `
   -title "Wezterm Neovim Launcher" -description "An executable that opens files in Neovim within a WezTerm terminal instance."
 
-git clone https://github.com/deltoss/PsInstallTools.git "$PSScriptRoot/temp/PsInstallTools"
-Import-Module -Verbose "$PSScriptRoot/temp/PsInstallTools"
-Install-FromFolder "$PSScriptRoot/output/" "WeztermNeovimLauncher"
+git clone https://github.com/deltoss/PsInstallTools.git "$scriptDir/temp/PsInstallTools"
+Import-Module -Verbose "$scriptDir/temp/PsInstallTools"
+Install-FromFolder "$scriptDir/output/" "WeztermNeovimLauncher"
 Remove-Module -Verbose PsInstallTools
-Remove-Item -Path "$PSScriptRoot/temp" -Recurse -Force
+Remove-Item -Path "$scriptDir/temp" -Recurse -Force
 
