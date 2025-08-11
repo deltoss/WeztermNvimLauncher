48cd05eecd58d68c6cb54445a18d0000bffb6b63
Michael Tran <mtran0011@outlook.com>
2025-08-11 18:08:00 +1000

    Adjusted install script

--- Install-App.ps1
+++ Install-App.ps1
@@ -17,9 +17,13 @@ New-Item -Path "$scriptDir/output" -ItemType Directory -Force
 ps2exe "$scriptDir/WeztermNeovimLauncher.ps1" "$scriptDir/output/WeztermNeovimLauncher.exe" -noConsole -iconFile "$scriptDir/assets/Icon.ico" -longPaths `
   -title "Wezterm Neovim Launcher" -description "An executable that opens files in Neovim within a WezTerm terminal instance."
 
-git clone https://github.com/deltoss/PsInstallTools.git "$scriptDir/temp/PsInstallTools"
-Import-Module -Verbose "$scriptDir/temp/PsInstallTools"
+# See https://github.com/deltoss/PsInstallTools
+$targetTempPath = "$Env:TEMP/PsInstallTools"
+if (Test-Path $targetTempPath) {
+    Remove-Item -Path $targetTempPath -Recurse -Force
+}
+git clone https://github.com/deltoss/PsInstallTools.git $targetTempPath
+Import-Module -Verbose $targetTempPath
 Install-FromFolder "$scriptDir/output/" "WeztermNeovimLauncher"
 Remove-Module -Verbose PsInstallTools
-Remove-Item -Path "$scriptDir/temp" -Recurse -Force
-
+Remove-Item -Path $targetTempPath -Recurse -Force
